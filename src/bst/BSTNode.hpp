c3a81222a1a97dc38dde3402db42cbe07f1c8616
Yifu Fang <yif003@ieng6.ucsd.edu>
2020-01-16 19:10:10 -0800

    BSTNode

--- src/bst/BSTNode.hpp
+++ src/bst/BSTNode.hpp
@@ -21,7 +21,11 @@ class BSTNode {
     BSTNode<Data>* parent;
 
     /** TODO */
-    BSTNode(const Data& d) : data(d) {}
+    BSTNode(const Data& d) : data(d) {
+        left = NULL;
+        right = NULL;
+        parent = NULL;
+    }
 
     /** Set the value of data */
     void setData(const Data& d) { data = d; }
@@ -30,7 +34,30 @@ class BSTNode {
     Data getData() { return data; }
 
     /** TODO */
-    BSTNode<Data>* successor() { return 0; }
+    BSTNode<Data>* successor() {
+        if (right != NULL) {
+            return right->LeftSuccessor();
+        } else if (parent == NULL) {
+            return 0;
+        } else if (this == parent->left) {
+            return parent;
+        } else
+            return parent->ParentSuccessor();
+    }
+    BSTNode<Data>* leftSuccessor() {
+        if (left != NULL)
+            return (left->leftSuccessor());
+        else
+            return this;
+    }
+    BSTNode<Data>* ParentSuccessor() {
+        if (parent == NULL)
+            return 0;
+        else if (this == parent->right)
+            return parent->ParentSuccessor();
+        else
+            return parent;
+    }
 };
 
 /**
